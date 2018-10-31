583c77c98b7855ccb55086825c5ea40e4128d29d
Shen <Fang-Wen.Shen@ascension.org>
2018-10-31 14:15:46 -0500

    Changes from Live Demo

--- Grunt-Ascension-Demonstration/wwwroot/lib/Gruntfile.js
+++ Grunt-Ascension-Demonstration/wwwroot/lib/Gruntfile.js
@@ -9,38 +9,38 @@ module.exports = function(grunt) {
             },
             files: ["../js/site.js"]
         },
-        //uglify: {
-        //    my_target: {
-        //        files: {
-        //            '../js/output.min.js': ['../js/site.js']
-        //        }
-        //    }
-        //},
-        //sass: {
-        //    dist: {
-        //        files: {
-        //            "../css/site.css":"../sass/main.scss"
-        //        }
-        //    }
-        //},
-        //watch: {
-        //    options: {
-        //        livereload: true
-        //    },
-        //    html: {
-        //        files: ["../../**/*.cshtml"]
-        //    },
-        //    javascripts: {
-        //        files: ["../js/*.js"],
-        //        tasks: ['jshint','uglify']
-        //    },
-        //    sass: {
-        //        files: ["../sass/*.scss"],
-        //        tasks: ['sass']
-        //    }
-        //}
+        uglify: {
+            my_target: {
+                files: {
+                    '../js/output.min.js': ['../js/site.js']
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    "../css/site.css":"../sass/main.scss"
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            html: {
+                files: ["../../**/*.cshtml"]
+            },
+            javascripts: {
+                files: ["../js/*.js"],
+                tasks: ['jshint','uglify']
+            },
+            sass: {
+                files: ["../sass/*.scss"],
+                tasks: ['sass']
+            }
+        }
     });
 
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-    grunt.registerTask('default', ['jshint'/*,'uglify'*//*, 'sass'*//*, 'watch'*/]);
+    grunt.registerTask('default', ['jshint','uglify', 'sass', 'watch']);
 };
